19ac2334ab1a16e1beb7ff9d7fdf4cdf69f1923b
Olga Spirina <olga.spirina11@gmail.com>
2023-05-06 10:51:18 +0200

    Logging and pytest works

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import argparse
+import os
+import logging
+
+import pytest
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, filename='/Users/grigorii/Projects/sync_folder_app/app.log')
+    logger = logging.getLogger("foo")
+    logger.info('Some msg')
+
+    logging.info("Some message")
+
     parser = argparse.ArgumentParser(description='cmd utility to sync two folders content')
 
     parser.add_argument('--src', required=True, help='source folder')
@@ -11,3 +21,23 @@ if __name__ == '__main__':
     args = vars(parser.parse_args())
 
     print(args)
+
+
+# @pytest.fixture(autouse=True)
+# def setup():
+#     pass
+#
+#
+# def tear_down():
+#     pass
+
+
+def test_smth(tmp_path):
+    log = logging.getLogger(__name__)
+    log.info('Some msg')
+
+    # print(f'tmp_path={tmp_path}')
+    d = tmp_path / "sub"
+    d.mkdir()
+
+    assert False
